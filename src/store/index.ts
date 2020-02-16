d9cb2a62109abc4628aaaf1688078362a2d9d8d7
mgsong <mgsong@jjsoft.kr>
2020-02-16 21:09:45 +0900

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {split, map} from 'ramda'
-import {go} from 'mingutils'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  getters: {
-  },
+  state: {},
+  mutations: {},
+  getters: {},
   actions: {},
   modules: {},
 })
