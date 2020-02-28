6c588d8947f3f902845ccd301ee5690ce81bbf24
mgsong <mgsong@jjsoft.kr>
2020-02-29 07:48:58 +0900

    show loading

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .main
+  .main(v-loading="state.loading")
     h4 이번 주 출석한 친구들({{$store.state.points.length}}/{{$store.state.students.length}})
     .list
       .student(v-for="(student, idx) in $store.state.students" :key="idx")
@@ -8,11 +8,27 @@
 
 <script lang="ts">
 import createLogger from 'if-logger'
-import {reactive, computed} from '@vue/composition-api'
+import {reactive, computed, watch} from '@vue/composition-api'
 
 const logger = createLogger().addTags('List.vue')
 export default {
   name: 'v-list',
+  setup(props, {root}) {
+    const state = reactive({
+      loading: true,
+    })
+    watch(
+      () => root.$store.state.students.length,
+      () => {
+        if (root.$store.state.students.length > 0) {
+          state.loading = false
+        }
+      },
+    )
+    return {
+      state,
+    }
+  },
 }
 </script>
 <style scoped lang="stylus">
