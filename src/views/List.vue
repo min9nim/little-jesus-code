bc2f5ac4be1adab632d47eed26687519f7127f65
mgsong <mgsong@jjsoft.kr>
2020-02-28 14:19:47 +0900

    출석완료 메뉴 추가

---
+++ src/views/List.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div 출석완료한 친구들 목록을 보여줄 예정..(작업 중)
+</template>
+
+<script lang="ts">
+export default {
+  name: 'v-list',
+}
+</script>
+<style scoped lang="stylus"></style>
