9842b45d096245f6a91654bf6dd9d3e2d90539cb
mgsong <mgsong@jjsoft.kr>
2020-03-03 07:03:24 +0900

    add component

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import Tag from 'element-ui/lib/tag'
 import Input from 'element-ui/lib/input'
 import Tabs from 'element-ui/lib/tabs'
 import TabPane from 'element-ui/lib/tab-pane'
+import Button from 'element-ui/lib/button'
+import loading from 'element-ui/lib/loading'
+import Timeline from 'element-ui/lib/timeline'
+
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 // import locale from 'element-ui/lib/locale/lang/ko'
@@ -21,6 +25,9 @@ Vue.use(Tag)
 Vue.use(Input)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Button)
+Vue.use(loading)
+Vue.use(Timeline)
 
 new Vue({
   router,
